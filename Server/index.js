177e87ec1c73167424091d1887916ed193b4f6b6
MichaelFeneder <michael.a.fender@outlook.com>
2021-11-29 13:22:00 -0600

    test

--- Server/index.js
+++ Server/index.js
@@ -11,7 +11,9 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static('client'))
 
-
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/index.html'))
+})
 
 
 
