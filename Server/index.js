12ab45049fc4baf668bdb44c33243c308532e04e
MichaelFeneder <michael.a.fender@outlook.com>
2021-11-29 13:14:43 -0600

    test

--- Server/index.js
+++ Server/index.js
@@ -26,7 +26,8 @@ app.put(`/api/snacks/:id`, updateSnack)
 
 
 ///////////////////////////////////////
-const port = process.env.PORT
+const port = process.env.PORT || 4000;
+
 app.listen(port, () => {
   console.log(`The app is ready on port ${port}`);
 });
